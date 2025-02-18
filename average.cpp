c4e1681083a523d3110abeb301d5754203fcdf3e
Pongpx <yannafong555@gmail.com>
2025-02-18 18:16:27 +0700

    Lab21 Finish

--- average.cpp
+++ average.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc,char *argv[]){
+    if(argc == 1) {cout << "Please input numbers to find average."; return 0;}
+    double sum;
+    for(int i=0; i<argc-1; i++){
+        double x = atof(argv[i+1]);
+        sum += x;
+    }
+    cout << "---------------------------------\n";
+    cout << "Average of " << argc-1;
+    cout << " numbers = " << sum/(argc-1);
+    cout << "\n---------------------------------";
+    return 0;
+}
--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,86 @@
-#include <windows.h>
 
+#include <windows.h>
+#include <stdio.h>
+
+HWND textfield, textBox1, textBox2;
+char text1Saved[100];
+char text2Saved[100];
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
-		
+
+		case WM_CREATE:
+			textfield = CreateWindow("STATIC", "Please input two numbers", WS_VISIBLE | WS_CHILD, 15, 20, 200, 25, hwnd, NULL, NULL, NULL);
+			textBox1 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD, 30, 50, 160, 25, hwnd, NULL, NULL, NULL);
+			textBox2 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD, 30, 80, 160, 25, hwnd, NULL, NULL, NULL);
+
+			CreateWindow("BUTTON", "+", WS_VISIBLE | WS_CHILD | WS_BORDER, 50, 120, 20, 20, hwnd, (HMENU) 1, NULL, NULL);
+			CreateWindow("BUTTON", "-", WS_VISIBLE | WS_CHILD | WS_BORDER, 80, 120, 20, 20, hwnd, (HMENU) 2, NULL, NULL);
+			CreateWindow("BUTTON", "*", WS_VISIBLE | WS_CHILD | WS_BORDER, 110, 120, 20, 20, hwnd, (HMENU) 3, NULL, NULL);
+			CreateWindow("BUTTON", "/", WS_VISIBLE | WS_CHILD | WS_BORDER, 140, 120, 20, 20, hwnd, (HMENU) 4, NULL, NULL);
+
+			break;
+
+		case WM_COMMAND:
+			switch (LOWORD(wParam)) {
+				case 1:
+				{
+					int gwtstat1 = 0;
+					gwtstat1 = GetWindowText(textBox1, &text1Saved[0], 100);
+
+					int gwtStat2 = 0;
+					gwtStat2 = GetWindowText(textBox2, &text2Saved[0], 100);
+
+					double num1 = atof(text1Saved);
+					double num2 = atof(text2Saved);
+					sprintf(text1Saved, "%lf", num1+num2);
+					::MessageBox(hwnd, text1Saved, "Result", MB_OK);
+				}
+					break;
+				case 2:
+				{
+					int gwtstat1 = 0;
+					gwtstat1 = GetWindowText(textBox1, &text1Saved[0], 100);
+
+					int gwtStat2 = 0;
+					gwtStat2 = GetWindowText(textBox2, &text2Saved[0], 100);
+
+					double num1 = atof(text1Saved);
+					double num2 = atof(text2Saved);
+					sprintf(text1Saved, "%lf", num1-num2);
+					::MessageBox(hwnd, text1Saved, "Result", MB_OK);
+				}
+					break;
+				case 3:
+				{
+					int gwtstat1 = 0;
+					gwtstat1 = GetWindowText(textBox1, &text1Saved[0], 100);
+
+					int gwtStat2 = 0;
+					gwtStat2 = GetWindowText(textBox2, &text2Saved[0], 100);
+
+					double num1 = atof(text1Saved);
+					double num2 = atof(text2Saved);
+					sprintf(text1Saved, "%lf", num1*num2);
+					::MessageBox(hwnd, text1Saved, "Result", MB_OK);
+				}
+					break;
+				case 4:
+				{
+					int gwtstat1 = 0;
+					gwtstat1 = GetWindowText(textBox1, &text1Saved[0], 100);
+
+					int gwtStat2 = 0;
+					gwtStat2 = GetWindowText(textBox2, &text2Saved[0], 100);
+
+					double num1 = atof(text1Saved);
+					double num2 = atof(text2Saved);
+					sprintf(text1Saved, "%lf", num1/num2);
+					::MessageBox(hwnd, text1Saved, "Result", MB_OK);
+				}
+					break;
+			}
+			break;
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
 			PostQuitMessage(0);
@@ -31,7 +108,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+25);
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,11 +118,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
